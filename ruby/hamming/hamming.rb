c973c3699443fb6efdeea89a06569f21c127034e
Greg Baugues <greg@baugues.com>
2014-03-31 22:32:18 -0500

    add giga

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,7 @@
 class Hamming
 
   def self.compute(a, b)
-    shortest_length(a,b).times.count { |i| a[i] != b[i] }
-  end
-
-  private
-
-  def self.shortest_length(a,b)
-    [a.size, b.size].min
+    [a, b].min.size.times.count { |i| a[i] != b[i] }
   end
 
 end
